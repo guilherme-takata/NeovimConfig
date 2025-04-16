871dfc37cf96dc7ca24982271b2205e0d8acf1d2
guilherme.takata <guilherme@gmail.com>
2025-04-16 00:30:36 -0300

    Add toggling of one dark styles

--- config/plugins/theme/onedark.nix
+++ config/plugins/theme/onedark.nix
@@ -2,7 +2,18 @@
   colorschemes = {
     onedark = {
       enable = true;
-      settings.style = "darker";
+      settings = {
+        style = "darker";
+        toggle_style_key = "<leader>ts";
+        toggle_style_list = [
+          "dark"
+          "darker"
+          "cool"
+          "deep"
+          "warm"
+          "warmer"
+        ];
+      };
     };
   };
 }
